f86f8dbfabd979566b331675e41d986b7118a55a
Administrator <Administrator@I91T1FM30VWNVX5>
2019-12-19 15:43:48 +0800

    lbt

---
+++ src/main/java/com/cgb1909/LbTnoteApplication.java
@@ -0,0 +1,13 @@
+package com.cgb1909;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LbTnoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LbTnoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cgb1909/LbTnoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgb1909;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LbTnoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
